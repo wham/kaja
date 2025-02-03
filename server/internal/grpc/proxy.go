2e2b38a7484faa123c3676cbcd66bc5e182adcde
Tomas Vesely <448809+wham@users.noreply.github.com>
2025-02-02 21:51:47 -0800

    ok proxy

--- server/internal/grpc/proxy.go
+++ server/internal/grpc/proxy.go
@@ -1,9 +1,9 @@
 package grpc
 
 import (
-	"bytes"
 	"context"
 	"encoding/base64"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"log/slog"
@@ -91,9 +91,39 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 
 	slog.Info("Received gRPC response", "length", len(res))
-	w.Header().Set("Content-Type", "application/grpc")
-	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(res)))
-	_, _ = io.Copy(w, bytes.NewReader(res))
+
+	// Build the gRPC-Web data frame:
+	// - 1 byte flag (0 for data frame)
+	// - 4 byte big-endian uint32 length
+	// - followed by the response message bytes
+	frame := make([]byte, 5+len(res))
+	frame[0] = 0 // flag for data frame
+	binary.BigEndian.PutUint32(frame[1:5], uint32(len(res)))
+	copy(frame[5:], res)
+
+	// Build the trailers frame containing the gRPC status code.
+	// The trailers frame has:
+	// - 1 byte flag (0x80 for trailers)
+	// - 4 byte big-endian uint32 length
+	// - trailers formatted as "key: value\r\n", ending with an extra "\r\n" line.
+	trailers := "grpc-status: 0\r\ngrpc-message: \r\n"
+	trailersBytes := []byte(trailers)
+	trailerFrame := make([]byte, 5+len(trailersBytes))
+	trailerFrame[0] = 0x80 // flag for trailer frame
+	binary.BigEndian.PutUint32(trailerFrame[1:5], uint32(len(trailersBytes)))
+	copy(trailerFrame[5:], trailersBytes)
+
+	// Concatenate data frame and trailer frame to form complete response.
+	fullResponse := append(frame, trailerFrame...)
+
+	// Optionally, if sending gRPC-Web text response, base64-encode the fullResponse:
+	encodedResponse := base64.StdEncoding.EncodeToString(fullResponse)
+
+	w.Header().Set("Content-Type", "application/grpc-web-text")
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(encodedResponse)))
+	if _, err := w.Write([]byte(encodedResponse)); err != nil {
+		slog.Error("Error writing response", "error", err)
+	}
 }
 
 func (p *Proxy) readGRPCWebMessage(r io.Reader, isText bool) ([]byte, error) {
