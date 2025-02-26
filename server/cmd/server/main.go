c4aa6f07ec7eed167704ae5405ecd8dc1829d28e
Tomas Vesely <448809+wham@users.noreply.github.com>
2025-02-25 20:06:00 -0800

    source

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -31,14 +31,14 @@ func handlerStubJs(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("CWD: %s\n", cwd)
 
 	// Read all files in the sources directory
-	sourcesDir := "./web/sources"
+	sourcesDir := "./build/sources"
 	var stubContent strings.Builder
 	err = filepath.Walk(sourcesDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if !info.IsDir() {
-			stubContent.WriteString("export * from \"" + strings.Replace(path, "web/sources/", "./", 1) + "\";\n")
+			stubContent.WriteString("export * from \"" + strings.Replace(path, "build/sources/", "./", 1) + "\";\n")
 		}
 		return nil
 	})
@@ -155,7 +155,7 @@ func main() {
 		})
 	}
 
-	mux.Handle("GET /sources/", http.StripPrefix("/sources/", http.FileServer(http.Dir("web/sources"))))
+	mux.Handle("GET /sources/", http.StripPrefix("/sources/", http.FileServer(http.Dir("build/sources"))))
 	mux.HandleFunc("GET /stub.js", handlerStubJs)
 	mux.HandleFunc("GET /status", handlerStatus)
 
