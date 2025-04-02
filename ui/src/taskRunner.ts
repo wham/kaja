adb7d0680f290a92eb26a219f659d7426985eee3
Tomas Vesely <448809+wham@users.noreply.github.com>
2025-04-01 21:51:06 -1000

    Full enum resolution

--- ui/src/taskRunner.ts
+++ ui/src/taskRunner.ts
@@ -4,22 +4,31 @@ import { Client, Project } from "./project";
 
 export function runTask(code: string, kaja: Kaja, projects: Project[]) {
   const file = ts.createSourceFile("task.ts", code, ts.ScriptTarget.Latest);
-  const clients: { [key: string]: Client } = {};
+  const args: { [key: string]: Client | Object } = {};
 
   file.statements.forEach((statement) => {
     if (ts.isImportDeclaration(statement)) {
-      const path = statement.moduleSpecifier.getText(file);
+      // slice(1, -1) - remove quotes
+      const path = statement.moduleSpecifier.getText(file).slice(1, -1)
       const project = projects.find((project) => path.includes(project.name));
       if (!project) {
+        return;
+      }
+      const source = project.sources.find((source) => source.importPath === path);
+      if (!source) {
         return;
       }
 
       const importClause = statement.importClause;
       if (importClause && importClause.namedBindings && ts.isNamedImports(importClause.namedBindings)) {
         importClause.namedBindings.elements.forEach((importSpecifier) => {
-          const name = importSpecifier.name.text;
+          const alias = importSpecifier.name.text;
+          const name = importSpecifier.propertyName ? importSpecifier.propertyName.text : alias;
           if (project.clients[name]) {
-            clients[name] = project.clients[name];
+            project.clients[name].kaja = kaja;
+            args[alias] = project.clients[name].methods;
+          } else if (source.enums[name]) {
+            args[alias] = source.enums[name].object;
           }
         });
       }
@@ -35,13 +44,9 @@ export function runTask(code: string, kaja: Kaja, projects: Project[]) {
     lines.shift();
   }
 
-  for (const client of Object.values(clients)) {
-    client.kaja = kaja;
-  }
-
   // Wrap the user's code in an async function so async keyword can be used
   const func = new Function(
-    ...Object.keys(clients),
+    ...Object.keys(args),
     "kaja",
     `
     return (async function() {
@@ -50,5 +55,5 @@ export function runTask(code: string, kaja: Kaja, projects: Project[]) {
   `,
   );
 
-  func(...Object.values(clients).map((client) => client.methods), kaja);
+  func(...Object.values(args), kaja);
 }
