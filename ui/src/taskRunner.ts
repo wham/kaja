325a47895fedc156a2e049537e718b900620c034
Tomas Vesely <448809+wham@users.noreply.github.com>
2025-04-02 12:40:57 -1000

    More efficient run

--- ui/src/taskRunner.ts
+++ ui/src/taskRunner.ts
@@ -1,15 +1,17 @@
 import ts from "typescript";
 import { Kaja } from "./kaja";
 import { Client, Project } from "./project";
+import { printStatements } from "./projectLoader";
 
 export function runTask(code: string, kaja: Kaja, projects: Project[]) {
   const file = ts.createSourceFile("task.ts", code, ts.ScriptTarget.Latest);
   const args: { [key: string]: Client | Object } = {};
+  const runStatements: ts.Statement[] = [];
 
   file.statements.forEach((statement) => {
     if (ts.isImportDeclaration(statement)) {
       // slice(1, -1) - remove quotes
-      const path = statement.moduleSpecifier.getText(file).slice(1, -1)
+      const path = statement.moduleSpecifier.getText(file).slice(1, -1);
       const project = projects.find((project) => path.includes(project.name));
       if (!project) {
         return;
@@ -32,17 +34,12 @@ export function runTask(code: string, kaja: Kaja, projects: Project[]) {
           }
         });
       }
+    } else {
+      runStatements.push(statement);
     }
   });
 
-  let lines = code.split("\n"); // split the code into lines
-
-  let isInImport = false;
-  // remove import statements
-  while (lines.length > 0 && (lines[0].startsWith("import ") || isInImport)) {
-    isInImport = !lines[0].endsWith(";");
-    lines.shift();
-  }
+  const runCode = printStatements(runStatements);
 
   // Wrap the user's code in an async function so async keyword can be used
   const func = new Function(
@@ -50,7 +47,7 @@ export function runTask(code: string, kaja: Kaja, projects: Project[]) {
     "kaja",
     `
     return (async function() {
-      ${lines.join("\n")}
+      ${runCode}
     })();
   `,
   );
