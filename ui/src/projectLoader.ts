7096b8b0b22fea632c07c42e294983b8f8656700
Tomas Vesely <448809+wham@users.noreply.github.com>
2025-04-01 19:33:51 -1000

    uvar

--- ui/src/projectLoader.ts
+++ ui/src/projectLoader.ts
@@ -13,7 +13,6 @@ export async function loadProject(paths: string[], configuration: ConfigurationP
   const services: Service[] = [];
 
   sources.forEach((source) => {
-    const sourceFile = source.file;
     const serviceInterfaceDefinitions: ts.VariableStatement[] = [];
 
     registerEnums(source);
