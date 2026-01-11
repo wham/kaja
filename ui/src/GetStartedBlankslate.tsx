97a168e52c8268730fc51302d978bf5863af5b9b
Tomáš Veselý <wham@users.noreply.github.com>
2026-01-10 19:36:21 -0800

    Used experimental Blankslate primer component (#150)

---
+++ ui/src/GetStartedBlankslate.tsx
@@ -0,0 +1,24 @@
+import { RocketIcon } from "@primer/octicons-react";
+import { Blankslate } from "@primer/react/experimental";
+
+export function GetStartedBlankslate() {
+  return (
+    <Blankslate
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+      }}
+    >
+      <Blankslate.Visual>
+        <RocketIcon size="medium" />
+      </Blankslate.Visual>
+      <Blankslate.Heading>Welcome to kaja</Blankslate.Heading>
+      <Blankslate.Description>
+        Select a method from the sidebar to get started.
+      </Blankslate.Description>
+    </Blankslate>
+  );
+}
