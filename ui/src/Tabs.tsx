bf4006e36396d2bf0764b4861c3d2c7d63e4a4c0
Tomas Vesely <448809+wham@users.noreply.github.com>
2026-01-22 23:17:30 -0800

    Changed scrollbar track to Monaco vs-dark background color

--- ui/src/Tabs.tsx
+++ ui/src/Tabs.tsx
@@ -85,7 +85,7 @@ export function Tabs({ children, activeTabIndex, onSelectTab, onCloseTab, onClos
           height: 2px;
         }
         .tabs-header::-webkit-scrollbar-track {
-          background-color: var(--bgColor-default);
+          background-color: #1e1e1e;
         }
         .tabs-header:hover::-webkit-scrollbar-thumb {
           background-color: var(--fgColor-muted);
