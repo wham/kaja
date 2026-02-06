ae16aa3c365b789bbf268b3475b9958818e65e64
Tomas Vesely <448809+wham@users.noreply.github.com>
2026-02-05 22:21:05 -0800

    Refactor TargetResult class for improved readability and structure

--- ui/src/wailsjs/go/models.ts
+++ ui/src/wailsjs/go/models.ts
@@ -1,18 +1,21 @@
 export namespace main {
-  export class TargetResult {
-    body: number[];
-    statusCode: number;
-    status: string;
+	
+	export class TargetResult {
+	    body: number[];
+	    statusCode: number;
+	    status: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new TargetResult(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.body = source["body"];
+	        this.statusCode = source["statusCode"];
+	        this.status = source["status"];
+	    }
+	}
 
-    static createFrom(source: any = {}) {
-      return new TargetResult(source);
-    }
-
-    constructor(source: any = {}) {
-      if ("string" === typeof source) source = JSON.parse(source);
-      this.body = source["body"];
-      this.statusCode = source["statusCode"];
-      this.status = source["status"];
-    }
-  }
 }
+
