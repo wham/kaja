3232e75fb4fde7c72d0642aa947858c5d106b4c2
Tomas Vesely <448809+wham@users.noreply.github.com>
2026-04-13 23:15:03 -0700

    Refactor whitespace in TargetResult class for improved readability

--- ui/src/wailsjs/go/models.ts
+++ ui/src/wailsjs/go/models.ts
@@ -1,14 +1,14 @@
 export namespace main {
-
+	
 	export class TargetResult {
 	    body: number[];
 	    statusCode: number;
 	    status: string;
-
+	
 	    static createFrom(source: any = {}) {
 	        return new TargetResult(source);
 	    }
-
+	
 	    constructor(source: any = {}) {
 	        if ('string' === typeof source) source = JSON.parse(source);
 	        this.body = source["body"];
